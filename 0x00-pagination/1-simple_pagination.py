cb82421ceac981675aa5abc027e669f6cf44b304
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2024-05-09 21:20:04 +0100

    simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,35 +1,48 @@
 #!/usr/bin/env python3
-"""Simple helper function"""
-
-from typing import Tuple, List
+"""
+Adds `get_page` method to `Server` class
+"""
 import csv
+from typing import List, Tuple
 
 
 class Server:
-    """Server class to paginate a database of popular baby names"""
-    
-        DATA_FILE = "Popular_Baby_Names.csv"
-    
-        def __init__(self):
-            """Initialize the class"""
-            self.__dataset = None
-    
-        def dataset(self) -> List[List]:
-            """Read the dataset and return it"""
-            if self.__dataset is None:
-                with open(self.DATA_FILE, "r") as f:
-                    reader = csv.reader(f)
-                    self.__dataset = [row for row in reader]
-            return self.__dataset
-        
-        @staticmethod
-        def index_range(page: int, page_size: int) -> Tuple[int, int]:
-            """Return a tuple of size two containing a start index and an end index"""
-            return ((page - 1) * page_size, page * page_size)
-    
-        def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """Get the requested page"""
-            assert isinstance(page, int) and page > 0
-            assert isinstance(page_size, int) and page_size > 0
-            start, end = self.index_range(page, page_size)
-            return self.dataset()[start:end]
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    @staticmethod
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+        """Calculate start and end index range for a `page`, with `page_size`
+        """
+        nextPageStartIndex = page * page_size
+        return nextPageStartIndex - page_size, nextPageStartIndex
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Get items for the given page number
+        Args:
+            page (int): page number
+            page_size (int): number of items per page
+        Returns:
+            (List[List]): a list of list(row) if inputs are within range
+            ([]) : an empty list if page and page_size are out of range
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        startIndex, endIndex = self.index_range(page, page_size)
+        return self.dataset()[startIndex:endIndex]
